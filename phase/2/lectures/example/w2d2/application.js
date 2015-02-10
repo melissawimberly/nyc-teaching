37363b4f9c7c493c6e318d657778660a5861a6ae
Zack Mance <zmance@gmail.com>
2015-02-10 10:11:53 -0500

    Adds code from live demo

--- phase/2/lectures/example/w2d2/application.js
+++ phase/2/lectures/example/w2d2/application.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 
 
+  var myCallback =  function(){
+    $('.btn-container').append( "<div class='hello'>Hello Squirrels 2015 </div>");
+  }
+
+  myCallback();
+  myCallback();
+
+
+  $('a#syHi').on('click', function(e){
+    console.log(e);
+    console.log(e.target);
+    console.log(this);
+    $('.hello').remove(); 
+  });
+
 });    
